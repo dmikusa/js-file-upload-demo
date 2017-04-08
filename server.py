d14cd196605b580197a46bdad732b194df5a07b9
Daniel Mikusa <dmikusa@pivotal.io>
2017-04-08 17:31:11 -0400

    Simple project on using HTML5 features for file selection and upload.

---
+++ server.py
@@ -0,0 +1,51 @@
+import os
+import logging
+from flask import Flask, request, send_from_directory, abort
+from werkzeug.utils import secure_filename
+
+logging.basicConfig(level=logging.INFO)
+
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def index():
+    return send_from_directory(os.getcwd(), 'index.html')
+
+
+@app.route('/uploads/<filename>')
+def uploads(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        logging.warn('no `file` input sent')
+        abort(400)
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit a empty part without filename
+    if file.filename == '':
+        logging.warn('no file selected by user')
+        abort(400)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return "OK", 201
+    abort(415)
+
+
+if __name__ == '__main__':
+    logging.info("Server starting...")
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5001)))
